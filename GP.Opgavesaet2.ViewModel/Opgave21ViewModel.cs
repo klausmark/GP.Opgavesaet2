75696cbd8369e3a2aa5c695d104b0fa9699e0047
Klaus V. Mark (KLMA) <klma@danishcrown.dk>
2016-04-04 11:34:09 +0200

    More design

--- GP.Opgavesaet2.ViewModel/Opgave21ViewModel.cs
+++ GP.Opgavesaet2.ViewModel/Opgave21ViewModel.cs
@@ -2,5 +2,15 @@
 {
     public class Opgave21ViewModel
     {
+        public string TilfældigeTal { get; set; }
+        public string TilfældigeTalIOmvendtRækkefølge { get; set; }
+        public string UdsnitAfDeTilfældigeTalUdFraPlaceringIArray { get; set; }
+        public int UdsnitAfDeTilfældigeTalUdFraPlaceringIArrayFra { get; set; }
+        public int UdsnitAfDeTilfældigeTalUdFraPlaceringIArrayTil { get; set; }
+        public string UdsnitAfDeTilfældigeTalUdFraHøjesteOgMindsteVærdier { get; set; }
+        public int UdsnitAfDeTilfældigeTalUdFraHøjesteOgMindsteVærdierMindsteVærdi { get; set; }
+        public int UdsnitAfDeTilfældigeTalUdFraHøjesteOgMindsteVærdierHøjesteVærdi { get; set; }
+        public int Antal { get; set; }
+        public int Føerste { get; set; }
     }
 }
